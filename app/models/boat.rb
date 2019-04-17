f21642748c8bc35dd81e5cd3395fbb6bb9e84d58
github username <github email address>
2019-04-16 21:40:39 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,37 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    first(5)
+  end
+
+  def self.dinghy
+    where("length < 20")
+  end
+
+  def self.ship
+    where("length >= 20")
+  end
+
+  def self.last_three_alphabetically
+    order(name: :desc).first(3)
+  end
+
+  def self.without_a_captain
+    where("captain_id IS NULL")
+  end
+
+  def self.sailboats
+    all.select {|boat| boat.classifications.include?(Classification.find_by(name: 'Sailboat'))}
+  end
+
+  def self.sailboats_with_captain
+    sailboats.reject { |sailboat| sailboat.captain.nil? }
+  end
+
+  def self.with_three_classifications
+    all.select {|boat| boat.classifications.length == 3}
+  end
 end
+
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,35 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def boat_types
+    boats.map { |boat| boat.classifications }.flatten.uniq
+  end
+
+  def self.catamaran_operators
+    all.select do |captain|
+      captain.boat_types.include?(Classification.find_by(name: 'Catamaran'))
+    end
+  end
+
+  def self.sailors
+    Boat.sailboats_with_captain.map {|sailboat| sailboat.captain }.uniq
+  end
+
+  def self.motorists
+    all.select do |captain|
+      captain.boat_types.include?(Classification.find_by(name: 'Motorboat'))
+    end
+  end
+
+  def self.talented_seafarers
+    all.select do |captain|
+      sailors.include?(captain) && motorists.include?(captain)
+    end
+  end
+
+  def self.non_sailors
+    all.reject do |captain|
+      captain.boat_types.include?(Classification.find_by(name: 'Sailboat'))
+    end
+  end
 end
